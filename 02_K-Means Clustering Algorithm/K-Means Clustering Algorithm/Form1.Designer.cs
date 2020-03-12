8d4513de4a7ca394c2f5c4aa1037c151817ed2db
Chalaka KasthuriArachchi <41720537+ChalakaKasthuriArachchi@users.noreply.github.com>
2020-03-13 02:12:21 +0530

    Add files via upload

---
+++ 02_K-Means Clustering Algorithm/K-Means Clustering Algorithm/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace K_Means_Clustering_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.txt_Clusters = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.list = new System.Windows.Forms.ListBox();
+            this.panel = new System.Windows.Forms.Panel();
+            this.btn_Clear = new MetroFramework.Controls.MetroButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.btn_Clear);
+            this.groupBox1.Controls.Add(this.metroButton1);
+            this.groupBox1.Controls.Add(this.txt_Clusters);
+            this.groupBox1.Controls.Add(this.metroLabel1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 639);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(649, 67);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Details";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(255, 23);
+            this.metroButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(259, 28);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "Mark Centroids";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // txt_Clusters
+            // 
+            this.txt_Clusters.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txt_Clusters.Location = new System.Drawing.Point(185, 23);
+            this.txt_Clusters.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Clusters.Name = "txt_Clusters";
+            this.txt_Clusters.Size = new System.Drawing.Size(62, 28);
+            this.txt_Clusters.TabIndex = 2;
+            this.txt_Clusters.Text = "3";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(8, 26);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(130, 20);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Number of Clusters";
+            // 
+            // list
+            // 
+            this.list.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 16;
+            this.list.Location = new System.Drawing.Point(901, 80);
+            this.list.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(259, 628);
+            this.list.TabIndex = 2;
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Location = new System.Drawing.Point(31, 80);
+            this.panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(862, 551);
+            this.panel.TabIndex = 3;
+            this.panel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_MouseClick);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(522, 23);
+            this.btn_Clear.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(112, 28);
+            this.btn_Clear.TabIndex = 5;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 722);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.list);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
+            this.Text = "K-Means Clustering Algorithm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox txt_Clusters;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Panel panel;
+        private MetroFramework.Controls.MetroButton btn_Clear;
+    }
+}
+
---
+++ 02_K-Means Clustering Algorithm/K-Means Clustering Algorithm/Form1.cs
@@ -0,0 +1,147 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K_Means_Clustering_Algorithm
+{
+    public partial class Form1 : MetroForm
+    {
+        Graphics g;
+        const int POINT_HEIGHT = 1;
+        const int LIMIT = 100000;
+        int n = 0;
+        int[] X = new int[LIMIT];
+        int[] Y = new int[LIMIT];
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel.CreateGraphics();
+        }
+
+        private void panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                g.DrawEllipse(Pens.Black, e.X, e.Y, POINT_HEIGHT, POINT_HEIGHT);
+                X[n] = e.X;
+                Y[n] = e.Y;
+                n++;
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            panel.Refresh(); // Refresh Panel to Remove Previos Drawings
+            for (int i = 0; i < n; i++) // Display Points on the Panel
+                g.DrawEllipse(Pens.Black, X[i], Y[i], POINT_HEIGHT, POINT_HEIGHT);
+            list.Items.Clear(); // Clear List
+
+            int clusters = 0;
+            try
+            {
+                clusters = int.Parse(txt_Clusters.Text);
+            }
+            catch 
+            {
+                MessageBox.Show("Invalid Inpu");
+                return;
+            }
+            double[] centroids_X = new double[clusters];
+            double[] centroids_Y = new double[clusters];
+            double[] prev_centroids_X = new double[clusters];
+            double[] prev_centroids_Y = new double[clusters];
+            int[,] nearest_points = new int[clusters, LIMIT];
+            int[] points_count = new int[clusters];
+
+            if (n < clusters)
+            {
+                MessageBox.Show("Not Enough Points");
+                return;
+            }
+            /* Mark Initial Centroids */
+            Random R = new Random();
+            for (int i = 0; i < clusters; i++)
+            {
+                centroids_X[i] = X[R.Next() % n];
+                centroids_Y[i] = Y[R.Next() % n];
+            }
+            bool Ended = false;
+            /* Infinite Loop - Until Centeroids Find */
+            for (int i = 0; !Ended ; i++) // i => Iteration 
+            {
+                for (int j = 0; j < n; j++) // j => Related Point
+                {
+                    double min = int.MaxValue;
+                    int nearest_cluster = 0;
+                    for (int k = 0; k < clusters; k++) // K => Cluster
+                    {
+                        double Distance = GetDistance(X[j], Y[j], centroids_X[k], centroids_Y[k]);
+                        if (Distance < min)
+                        {
+                            min = Distance;
+                            nearest_cluster = k;
+                        }
+                    }
+                    nearest_points[nearest_cluster, points_count[nearest_cluster]++] = j;
+                }
+                /* Finalizing Iteration */
+                list.Items.Add("Iteration : " + (i + 1));
+                for (int m = 0; m < clusters; m++) //m => cluster
+                {
+                    double sumX = 0;
+                    double sumY = 0;
+                    for (int n = 0; n < points_count[m]; n++) //n => Nearest Points to the Cluster {m}
+                    {
+                        sumX += X[nearest_points[m, n]];
+                        sumY += Y[nearest_points[m, n]];
+                    }
+                    if(points_count[m] == 0)
+                    {
+                        MessageBox.Show("Randomly Selected Points do not match with the Data Set\n" +
+                            "Please Restart the Process");
+                        return;
+                    }
+                    centroids_X[m] = sumX / points_count[m];
+                    centroids_Y[m] = sumY / points_count[m];
+                    list.Items.Add(String.Format("C{0} : [ X => {1:000.00} ]  [ Y => {2:000.00} ]", m + 1,
+                        centroids_X[m], centroids_Y[m]));
+                    points_count[m] = 0;
+                }
+                /* Check Whether Were the  Positions of Centroids changed  
+                   related to the previous step*/
+                bool Flag = true;
+                for (int m = 0; m < clusters; m++) //m => cluster
+                {
+                    if (!Flag || centroids_X[m] != prev_centroids_X[m])
+                        Flag = false;
+                    if (!Flag || centroids_X[m] != prev_centroids_X[m])
+                        Flag = false;
+                    prev_centroids_X[m] = centroids_X[m];
+                    prev_centroids_Y[m] = centroids_Y[m];
+                }
+                Ended = Flag;
+            }
+            for (int m = 0; m < clusters; m++) //m => cluster
+            {
+                g.FillEllipse(Brushes.Red, (float)centroids_X[m], (float)centroids_Y[m], POINT_HEIGHT * 5, POINT_HEIGHT * 5);
+            }
+        }
+        private static double GetDistance(double x1,double y1,double x2,double y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            panel.Refresh();
+            n = 0;
+        }
+    }
+}
